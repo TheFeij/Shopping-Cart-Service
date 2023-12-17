8aa28c4825512a4c82d492df9ac01deb92ac20ad
thefeij <abolfazl.moradi.feijani@gmail.com>
2023-12-17 19:36:50 +0330

    Defining DB and Init() function

--- db/db.go
+++ db/db.go
@@ -1,1 +1,22 @@
 package db
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
+func Init() {
+	dsn := "host=localhost port=5432 user=root password=1234 dbname=shoping_cart_db sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//TODO: migration of the db models
+
+	DB = db
+}
