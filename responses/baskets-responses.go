49b3ac92bb24198d0bc1923b9a93f07fc200320e
thefeij <abolfazl.moradi.feijani@gmail.com>
2023-12-18 09:37:57 +0330

    Updated the response structs to use models.Basket

--- responses/baskets-responses.go
+++ responses/baskets-responses.go
@@ -2,17 +2,12 @@ package responses
 
 import (
 	"Shoping-Cart-Service/db/models"
-	"time"
 )
 
 type GetBasketsListResponse struct {
-	Baskets []GetBasketResponse `json:"baskets"`
+	Baskets []models.Basket `json:"baskets"`
 }
 
 type GetBasketResponse struct {
-	ID        uint         `json:"id"`
-	CreatedAt time.Time    `json:"createdAt"`
-	UpdatedAt time.Time    `json:"updatedAt"`
-	Data      []byte       `json:"data"`
-	State     models.State `json:"state"`
+	models.Basket
 }
