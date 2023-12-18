d7adc2ceb11c009a805fd7cd494636c007ab6b1b
thefeij <abolfazl.moradi.feijani@gmail.com>
2023-12-18 08:48:32 +0330

    Defined response structs for getting a basket and getting list of all baskets

---
+++ responses/baskets-responses.go
@@ -0,0 +1,18 @@
+package responses
+
+import (
+	"Shoping-Cart-Service/db/models"
+	"time"
+)
+
+type GetBasketsListResponse struct {
+	Baskets []models.Basket `json:"baskets"`
+}
+
+type GetBasketResponse struct {
+	ID        uint         `json:"id"`
+	CreatedAt time.Time    `json:"createdAt"`
+	UpdatedAt time.Time    `json:"updatedAt"`
+	Data      []byte       `json:"data"`
+	State     models.State `json:"state"`
+}
